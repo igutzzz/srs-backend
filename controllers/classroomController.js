e74206c5fa34b63199f7d8da75d747aa9c43a790
Igor Almeida <igor.almeida@madeinweb.app>
2024-05-03 19:25:00 -0300

    fix: classroom return

--- controllers/classroomController.js
+++ controllers/classroomController.js
@@ -5,69 +5,80 @@ export async function getClassrooms(request, reply) {
     if (error) {
       throw error;
     }
-    return reply.code(200).send(JSON.stringify(data));
+    const groupedClassrooms = data.reduce((acc, classroom) => {
+      const { floor, id, name, schoolId, created_at, items } = classroom;
+      if (!acc[floor]) {
+        acc[floor] = [];
+      }
+      acc[floor].push({ id, name, floor, schoolId, created_at, items });
+      return acc;
+    }, {});
+    return reply.code(200).send(JSON.stringify(groupedClassrooms));
   } catch (err) {
     return reply.code(500).send(err);
   }
 }
 export async function getClassroom(request, reply) {
-    try {
-      const { data, error } = await supabase
-        .from("classroom")
-        .select()
-        .eq("id", request.params.params);
-      if (error) {
-        throw error;
-      }
-      return reply.code(200).send(JSON.stringify(data));
-    } catch (err) {
-      return reply.code(500).send(err);
+  try {
+    const { data, error } = await supabase
+      .from("classroom")
+      .select()
+      .eq("id", request.params.params);
+    if (error) {
+      throw error;
     }
+
+    return reply.code(200).send(JSON.stringify(data));
+  } catch (err) {
+    return reply.code(500).send(err);
   }
-  
-  export async function postClassroom(request, reply) {
-    var response;
-    try {
-      const { data, error } = await supabase.from("classroom").insert(request.body);
-      if (error) {
-        throw error;
-      }
-      reply.code(200).send({ data, error });
-    } catch (err) {
-      reply.code(500).send(err);
+}
+
+export async function postClassroom(request, reply) {
+  var response;
+  try {
+    const { data, error } = await supabase
+      .from("classroom")
+      .insert(request.body);
+    if (error) {
+      throw error;
     }
+    reply.code(200).send({ data, error });
+  } catch (err) {
+    reply.code(500).send(err);
   }
-  
-  export async function updateClassroom(request, reply) {
-    try {
-      var id = request.params.params;
-      const { data, error } = await supabase
-        .from("classroom")
-        .update(request.body)
-        .eq("id", id)
-        .select();
-      if (error) {
-        throw error;
-      }
-      reply.code(200).send(data);
-    } catch (err) {
-      reply.code(400).send(err);
+}
+
+export async function updateClassroom(request, reply) {
+  try {
+    var id = request.params.params;
+    const { data, error } = await supabase
+      .from("classroom")
+      .update(request.body)
+      .eq("id", id)
+      .select();
+    if (error) {
+      throw error;
     }
+    reply.code(200).send(data);
+  } catch (err) {
+    reply.code(400).send(err);
   }
-  
-  export async function deleteClassroom(request, reply) {
-    try {
-      var id = request.params.params;
-      const { data, error } = await supabase
-        .from("classroom")
-        .delete()
-        .eq("id", id)
-        .select();
-      if (error) {
-        throw error;
-      }
-      reply.code(200).send(data);
-    } catch (err) {
-      reply.code(400).send(err);
+}
+
+export async function deleteClassroom(request, reply) {
+  try {
+    var id = request.params.params;
+    const { data, error } = await supabase
+      .from("classroom")
+      .delete()
+      .eq("id", id)
+      .select();
+    if (error) {
+      throw error;
     }
+    reply.code(200).send(data);
+  } catch (err) {
+    reply.code(400).send(err);
   }
+}
