922a89d925bff463252abf9f3c301f3b070f1e6f
GGNOOB1 <geraldovicente987@gmail.com>
2024-06-02 16:29:48 -0300

    add: Adding new routes for classes

--- routes/class.js
+++ routes/class.js
@@ -4,11 +4,19 @@ import {
   postClass,
   updateClass,
   deleteClass,
+  getClassesByTeacherId,
+  getCoursesByClassByTeacherId,
+  getClassesByCourseIdByTeacherId,
+  getClassesByTeacherIdByWeekDay
 } from "../controllers/classController.js";
 
 export default async function classRoutes(server) {
   server.get("/classes", getClasses);
   server.get("/class/:params", getClass);
+  server.get("/classes/:params", getClassesByTeacherId);
+  server.get("/classes/:teacherId/:weekDay", getClassesByTeacherIdByWeekDay);
+  server.get("/classes/courses/:params", getCoursesByClassByTeacherId);
+  server.get("/classes/courses/:courseId/teacher/:teacherId", getClassesByCourseIdByTeacherId);
   server.post("/class", postClass);
   server.put("/class/:params", updateClass);
   server.delete("/class/:params", deleteClass);
