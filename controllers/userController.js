d65b2ee9199f81eb9c2b7f8a45007af320c94bb7
igutzzz <igor.almeida@madeinweb.app>
2024-06-06 00:30:22 -0300

    add bcrypt

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 import supabase from "../supabaseConfig.js";
+import bcrypt from 'bcrypt';
 export async function getUsers(request, reply) {
   try {
     const { data, error } = await supabase.from("user").select();
@@ -11,10 +12,11 @@ export async function getUsers(request, reply) {
   }
 }
 export async function getUser(request, reply) {
-    try {
-      const { data, error } = await supabase
-        .from("user")
-        .select(`
+  try {
+    const { data, error } = await supabase
+      .from("user")
+      .select(
+        `
           firstName,
           lastName,
           userStudent(
@@ -28,60 +30,61 @@ export async function getUser(request, reply) {
             ra
             
           )
-        `)
-        .eq("id", request.params.params);
-      if (error) {
-        throw error;
-      }
-      return reply.code(200).send(JSON.stringify(data));
-    } catch (err) {
-      return reply.code(500).send(err);
+        `
+      )
+      .eq("id", request.params.params);
+    if (error) {
+      throw error;
     }
+    return reply.code(200).send(JSON.stringify(data));
+  } catch (err) {
+    return reply.code(500).send(err);
   }
-  
-  export async function postUser(request, reply) {
-    var response;
-    try {
-      const { data, error } = await supabase.from("user").insert(request.body);
-      if (error) {
-        throw error;
-      }
-      reply.code(200).send({ data, error });
-    } catch (err) {
-      reply.code(500).send(err);
+}
+
+export async function postUser(request, reply) {
+  console.log(request.body)
+  try {
+    const { data, error } = await supabase.from("user").insert(request.body);
+     if (error) {
+     throw error;
     }
+    reply.code(200).send({ data, error });
+  } catch (err) {
+    reply.code(500).send(err);
   }
-  
-  export async function updateUser(request, reply) {
-    try {
-      var id = request.params.params;
-      const { data, error } = await supabase
-        .from("user")
-        .update(request.body)
-        .eq("id", id)
-        .select();
-      if (error) {
-        throw error;
-      }
-      reply.code(200).send(data);
-    } catch (err) {
-      reply.code(400).send(err);
+}
+
+export async function updateUser(request, reply) {
+  try {
+    var id = request.params.params;
+    const { data, error } = await supabase
+      .from("user")
+      .update(request.body)
+      .eq("id", id)
+      .select();
+    if (error) {
+      throw error;
     }
+    reply.code(200).send(data);
+  } catch (err) {
+    reply.code(400).send(err);
   }
-  
-  export async function deleteUser(request, reply) {
-    try {
-      var id = request.params.params;
-      const { data, error } = await supabase
-        .from("user")
-        .delete()
-        .eq("id", id)
-        .select();
-      if (error) {
-        throw error;
-      }
-      reply.code(200).send(data);
-    } catch (err) {
-      reply.code(400).send(err);
+}
+
+export async function deleteUser(request, reply) {
+  try {
+    var id = request.params.params;
+    const { data, error } = await supabase
+      .from("user")
+      .delete()
+      .eq("id", id)
+      .select();
+    if (error) {
+      throw error;
     }
+    reply.code(200).send(data);
+  } catch (err) {
+    reply.code(400).send(err);
   }
+}
